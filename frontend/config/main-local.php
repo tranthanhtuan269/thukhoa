73dabcf50bb1ad08ee0b361d30701bc5dff5560f
tran.thanh.tuan269 <tran.thanh.tuan269@gmail.com>
2015-10-14 11:18:35 +0700

    first commit

---
+++ frontend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'mFRVkLr4QDZb9IwdoAciv_Ol2QrGcsP8',
+        ],
+    ],
+];
